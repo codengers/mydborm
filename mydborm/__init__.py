738822a30d5d818acb5da8aa393e2cab9dd2c290
codengers <codengers@gmail.com>
2026-06-18 21:27:47 +0530

    bump: version 0.5.0 -> 0.6.0

--- mydborm/__init__.py
+++ mydborm/__init__.py
@@ -58,7 +58,7 @@ from .exceptions import (
 from .bulk import BulkResult, chunked_bulk_create, chunked_bulk_update, chunked_bulk_delete
 from .session import Session, ObjectState
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 __author__  = "Codengers"
 __license__ = "MIT"
 
